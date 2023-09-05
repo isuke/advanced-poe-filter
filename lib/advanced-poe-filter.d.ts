127831649117866fbd380b0f69a0c74d964a90e6
isuke <isuke770@gmail.com>
2023-09-05 13:55:04 +0900

    feat(type): add types

--- lib/advanced-poe-filter.d.ts
+++ lib/advanced-poe-filter.d.ts
@@ -1,4 +1,4 @@
-import { AdvancedBlock } from "advanced-poe-filter-parser"
+import type { AdvancedBlock, SyntaxError as ParserSyntaxError } from "advanced-poe-filter-parser"
 
 type Block = {
   id: AdvancedBlock["id"]
@@ -24,6 +24,7 @@ type Options = {
   initialFontSize: number
 }
 
-export type version = `{number}.{number}.{number}`
+declare const version: `{number}.{number}.{number}`
+export type SyntaxError = ParserSyntaxError
 export function getObject(advancedScriptText: string, variables?: object, properties?: object, _filterInfo?: object, originalOptions?: Options): PoeFilterObject
 export function compile(advancedScriptText: string, variables?: object, properties?: object, filterInfo?: object, originalOptions?: Options): string
